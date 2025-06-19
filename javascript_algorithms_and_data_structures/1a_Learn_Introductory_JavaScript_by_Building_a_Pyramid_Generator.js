2b9d9a9460ef341d4241e1515218dc9892128fc8
Bryan <89350478+sabahbryan@users.noreply.github.com>
2025-06-19 20:18:04 +0800

    pyramid2
    declare variable "character"

--- javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
+++ javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
@@ -1,6 +1,1 @@
-console.log("Let's build a pyramid using JavaScript basics:");
-console.log("    #    ");
-console.log("   ###   ");
-console.log("  #####  ");
-console.log(" ####### ");
-console.log("#########");
+let character;
