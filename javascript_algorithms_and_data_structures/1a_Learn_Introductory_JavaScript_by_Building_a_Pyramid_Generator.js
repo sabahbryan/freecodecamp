6ed867432e92b6b6361a0ad8a3376b20bdf497d7
Bryan <89350478+sabahbryan@users.noreply.github.com>
2025-06-25 21:52:52 +0800

    pyramid29
    remove previous step

--- javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
+++ javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
@@ -1,9 +1,8 @@
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
-rows.push("freeCodeCamp");
-let pushed = rows.push();
-console.log(pushed);
-let popped = rows.pop();
-console.log(popped);
-console.log(rows);
+// let pushed = rows.push("freeCodeCamp");
+// console.log(pushed);
+// let popped = rows.pop();
+// console.log(popped);
+// console.log(rows);
