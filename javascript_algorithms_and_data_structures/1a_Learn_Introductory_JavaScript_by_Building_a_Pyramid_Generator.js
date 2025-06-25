a414a3bb013798fdb637fe85dc88ec6dd8ab324a
Bryan <89350478+sabahbryan@users.noreply.github.com>
2025-06-25 19:38:18 +0800

    pyramid17
    test with different operations

--- javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
+++ javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
@@ -1,3 +1,6 @@
 let character = 'Hello';
 let count = 8;
-console.log(count + 1);
+//console.log(count + 1); = 9
+//console.log(count - 1); = 7
+//console.log(count * 2); = 16
+//console.log(count / 2); = 4
