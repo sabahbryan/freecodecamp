59e641676a27a2a06070d033b5ece4a7173af139
Bryan <89350478+sabahbryan@users.noreply.github.com>
2025-06-25 21:48:35 +0800

    pyramid27
    use "pop" to remove last element from array, then return it

--- javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
+++ javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
@@ -2,4 +2,6 @@ let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
 rows.push("freeCodeCamp");
+let popped = rows.pop();
+console.log(popped);
 console.log(rows);
