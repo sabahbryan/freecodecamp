189a0e0d3315b6b80b3fb1f76474695a7c895631
Bryan <89350478+sabahbryan@users.noreply.github.com>
2025-06-25 21:51:00 +0800

    pyramid28
    use "push" to return the new length of the array

--- javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
+++ javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
@@ -2,6 +2,8 @@ let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
 rows.push("freeCodeCamp");
+let pushed = rows.push();
+console.log(pushed);
 let popped = rows.pop();
 console.log(popped);
 console.log(rows);
