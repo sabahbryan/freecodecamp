c6f44269a0eb8d42b45a5494e9d8c0fbe87d1cf2
Bryan <89350478+sabahbryan@users.noreply.github.com>
2025-07-07 18:13:00 +0800

    pyramid55
    declare addTwoNumbers function that takes two numbers, sums them, then returns the value (sum)

--- javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
+++ javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
@@ -5,7 +5,13 @@ const rows = [];
 function padRow(name) {
   return name;
 }
-const call = padRow("Bryan");
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+const sum = addTwoNumbers(5, 10);
+console.log(sum);
+
+const call = padRow("CamperChan");
 console.log(call);
 
 
