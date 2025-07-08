e3068f297a0f938d284a4bb805bfbcf7af52024d
Bryan <89350478+sabahbryan@users.noreply.github.com>
2025-07-08 21:49:26 +0800

    pyramid83
    add "else" block
    *change 2nd statement to "false" to display "else" block statement

--- javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
+++ javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
@@ -13,9 +13,10 @@ function padRow(rowNumber, rowCount) {
 
 if ("") {
   console.log("Condition is true");
-}
-else if (5 < 10) {
+} else if (5 < 10) {
   console.log("5 is less than 10");
+} else {
+  console.log("This is the else block");
 }
 
 let result = ""
