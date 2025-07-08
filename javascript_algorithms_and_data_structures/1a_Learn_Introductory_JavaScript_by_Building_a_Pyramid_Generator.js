092b60e1de90ae9b5ce8b5d8b79219cc9834fa02
Bryan <89350478+sabahbryan@users.noreply.github.com>
2025-07-08 20:52:47 +0800

    pyramid76
    add comment reminder

--- javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
+++ javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
@@ -6,6 +6,7 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
+// TODO: use a different type of loop
 for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count));
 }
