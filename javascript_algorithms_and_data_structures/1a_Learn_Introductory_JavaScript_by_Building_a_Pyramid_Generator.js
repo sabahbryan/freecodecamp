8d4ef8893133cf18f00a23185815b9d50ddec584
Bryan <89350478+sabahbryan@users.noreply.github.com>
2025-07-08 17:06:31 +0800

    pyramid63
    remove call declaration and its console log

--- javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
+++ javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
@@ -3,11 +3,9 @@ const count = 8;
 const rows = [];
 
 function padRow() {
-  
-  
+
 }
-const call = padRow();
-console.log(call);
+
 
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character.repeat(i + 1))
