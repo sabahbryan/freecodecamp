220dc7279abccf8732d00476caa6c2280b460aed
Bryan <89350478+sabahbryan@users.noreply.github.com>
2025-07-08 17:04:00 +0800

    pyramid61
    remove elements from padRow and the respective console.log(s)

--- javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
+++ javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
@@ -2,16 +2,14 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow(name) {
+function padRow() {
   const test = "Testing";
-  console.log("This works!")
+
   return test;
-  console.log("This works!")
+
 }
-
-const call = padRow("CamperChan");
+const call = padRow();
 console.log(call);
-
 
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character.repeat(i + 1))
