b908f423bf26f73531186a71c851c3b41f4cc993
Bryan <89350478+sabahbryan@users.noreply.github.com>
2025-07-10 20:46:02 +0800

    pyramid112
    declare "inverted" variable to be true
    *use "let" instead of "const" to allow it to be modified

--- javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
+++ javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
@@ -1,6 +1,7 @@
 const character = "#";
 const count = 8;
 const rows = [];
+let inverted = true;
 
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
