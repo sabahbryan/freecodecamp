da11a8bcde331a07c6b4b47a9d50cc29cda136be
Bryan <89350478+sabahbryan@users.noreply.github.com>
2025-07-10 20:52:46 +0800

    pyramid116
    add .push in the "else" block
    *opposite of .unshift (inverted)

--- javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
+++ javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
@@ -11,9 +11,8 @@ function padRow(rowNumber, rowCount) {
 for (let i = 1; i <= count; i++) {
   if (inverted) {
     rows.unshift(padRow(i, count));
-  }
-  else {
-    
+  } else {
+    rows.push(padRow(i, count));
   }
 }
 
