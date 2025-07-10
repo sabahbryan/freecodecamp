4eeb21b4869cf993c78e76821696690007ad28a0
Bryan <89350478+sabahbryan@users.noreply.github.com>
2025-07-10 20:43:10 +0800

    pyramid110
    uncomment first "for" loop

--- javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
+++ javascript_algorithms_and_data_structures/1a_Learn_Introductory_JavaScript_by_Building_a_Pyramid_Generator.js
@@ -7,9 +7,9 @@ function padRow(rowNumber, rowCount) {
 }
 
 // TODO: use a different type of loop
-/*for (let i = 1; i <= count; i++) {
+for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count));
-}*/
+}
 
 /*while (rows.length < count) {
   rows.push(padRow(rows.length + 1, count));
@@ -18,13 +18,6 @@ function padRow(rowNumber, rowCount) {
 /*for (let i = count; i > 0; i--) {
   rows.push(padRow(i, count));
 }*/
-
-// const numbers = [1, 2, 3];
-// const shifted = numbers.shift()
-// console.log(shifted);
-// const unshifted = numbers.unshift(5);
-// console.log(unshifted);
-// console.log(numbers);
 
 let result = ""
 
