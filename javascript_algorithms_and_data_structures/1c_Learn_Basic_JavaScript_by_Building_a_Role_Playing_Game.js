3da861edc2aceb61881f6c5ee26bde77bc1a2d9d
Bryan <89350478+sabahbryan@users.noreply.github.com>
2025-08-08 17:33:17 +0800

    rpg39
    add "innerText" for button2 & button3 for goStore function
    *changes text for button2 & button3

--- javascript_algorithms_and_data_structures/1c_Learn_Basic_JavaScript_by_Building_a_Role_Playing_Game.js
+++ javascript_algorithms_and_data_structures/1c_Learn_Basic_JavaScript_by_Building_a_Role_Playing_Game.js
@@ -23,7 +23,9 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function goStore() {
-  button1.innerText = "Buy 10 health (10 gold)"
+  button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
 }
 
 function goCave() {
