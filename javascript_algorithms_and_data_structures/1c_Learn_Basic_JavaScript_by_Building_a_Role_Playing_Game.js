ec501bca25715d404c124f34e54bb3207229ce56
Bryan <89350478+sabahbryan@users.noreply.github.com>
2025-08-13 15:36:09 +0800

    rpg43
    move "goTown" up and duplicate code of "goStore" function into "goTown"

--- javascript_algorithms_and_data_structures/1c_Learn_Basic_JavaScript_by_Building_a_Role_Playing_Game.js
+++ javascript_algorithms_and_data_structures/1c_Learn_Basic_JavaScript_by_Building_a_Role_Playing_Game.js
@@ -21,6 +21,15 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+function goTown() {
+  button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+  text.innerText = "You enter the store.";
+}
 
 function goStore() {
   button1.innerText = "Buy 10 health (10 gold)";
@@ -45,9 +54,5 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-  
-}
 
-function goTown() {
-  
 }
