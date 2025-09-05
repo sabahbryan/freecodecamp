7f0d7a4e1564b449e226551f336cf4c4ead71ce5
Bryan <89350478+sabahbryan@users.noreply.github.com>
2025-09-05 18:11:48 +0800

    rpg64
    changed the value of the "button1.innerText" assignment to be "location["button text"]"

--- javascript_algorithms_and_data_structures/1c_Learn_Basic_JavaScript_by_Building_a_Role_Playing_Game.js
+++ javascript_algorithms_and_data_structures/1c_Learn_Basic_JavaScript_by_Building_a_Role_Playing_Game.js
@@ -37,7 +37,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
-  button1.innerText = "Go to store";
+  button1.innerText = location["button text"];
   button2.innerText = "Go to cave";
   button3.innerText = "Fight dragon";
   button1.onclick = goStore;
@@ -51,13 +51,7 @@ function goTown() {
 }
 
 function goStore() {
-//   button1.innerText = "Buy 10 health (10 gold)";
-//   button2.innerText = "Buy weapon (30 gold)";
-//   button3.innerText = "Go to town square";
-//   button1.onclick = buyHealth;
-//   button2.onclick = buyWeapon;
-//   button3.onclick = goTown;
-//   text.innerText = "You enter the store.";
+
 }
 
 function goCave() {
