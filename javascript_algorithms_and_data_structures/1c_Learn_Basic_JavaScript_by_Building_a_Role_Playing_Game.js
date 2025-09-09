db34977ec37fd7db7ecb71a4f2b294fe13c690cc
Bryan <89350478+sabahbryan@users.noreply.github.com>
2025-09-09 17:54:02 +0800

    rpg94
    add else statement with text for "insufficient gold"

--- javascript_algorithms_and_data_structures/1c_Learn_Basic_JavaScript_by_Building_a_Role_Playing_Game.js
+++ javascript_algorithms_and_data_structures/1c_Learn_Basic_JavaScript_by_Building_a_Role_Playing_Game.js
@@ -95,6 +95,9 @@ function buyWeapon() {
     inventory.push(newWeapon);
     text.innerText += " In your inventory you have: " + inventory;
   }
+  else {
+    text.innerText = "You do not have enough gold to buy a weapon."
+  }
 }
 
 function fightSlime() {
