567521b78bd5dd43b53d0d3677ceb1e670ba61d3
Bryan <89350478+sabahbryan@users.noreply.github.com>
2025-09-12 21:16:12 +0800

    rpg115
    add "fight" object in "locations" array

--- javascript_algorithms_and_data_structures/1c_Learn_Basic_JavaScript_by_Building_a_Role_Playing_Game.js
+++ javascript_algorithms_and_data_structures/1c_Learn_Basic_JavaScript_by_Building_a_Role_Playing_Game.js
@@ -57,6 +57,12 @@ const locations = [
     "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
     "button functions": [fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters."
+  },
+  {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster."
   }
 ];
 
@@ -154,5 +160,5 @@ function attack() {
 }
 
 function dodge() {
-  
+
 }
