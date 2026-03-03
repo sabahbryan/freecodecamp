013df72a0d5de49e3e732a64fd71800f3d753c33
Bryan <89350478+sabahbryan@users.noreply.github.com>
2026-03-03 17:42:55 +0800

    rpg150
    create if statement to call isMonsterHit function and wrap monsterHealth variable inside

--- javascript_algorithms_and_data_structures/1c_Learn_Basic_JavaScript_by_Building_a_Role_Playing_Game.js
+++ javascript_algorithms_and_data_structures/1c_Learn_Basic_JavaScript_by_Building_a_Role_Playing_Game.js
@@ -182,7 +182,9 @@ function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
   health -= getMonsterAttackValue(monsters[fighting].level);
-  monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  if (isMonsterHit()) {
+    monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  }
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
   if (health <= 0) {
@@ -199,8 +201,7 @@ function attack() {
 function getMonsterAttackValue(level) {
   const hit = (level * 5) - (Math.floor(Math.random() * xp));
   console.log(hit);
-  //return hit;
-  return hit > 0 ? hit : 0
+  return hit > 0 ? hit : 0;
 }
 
 function dodge() {
